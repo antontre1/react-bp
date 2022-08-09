bbd6678fc75f079f013b350712b12f628f4a7ebd
Antonin Folliasson <antonin@aitio.co>
2022-08-09 10:12:34 +0200

    effective with react svg component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+// import Bell from './bell.svg'
 
 function App() {
   return (
     <div className="App">
       <h1>My Full Page</h1>
+      {/* <Bell /> */}
     </div>
   )
 
