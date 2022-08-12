e7f2232137914613ce7de2dbda341e12b1087eec
Antonin Folliasson <antonin@aitio.co>
2022-08-12 15:07:05 +0200

    quick fix for css

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin');
 
 module.exports = {
   entry: ['./src/',],
-  mode: 'production',
+  mode: 'development',
   devServer: {
     // if 404 it serves index as an answer
     historyApiFallback: true,
     open: true,
     compress: true,
     port: 8080,
-    hot: true
+    hot: true,
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -33,32 +33,32 @@ module.exports = {
         }
       },
       // {
-        //   test: /\.(png|jpg)$/i,
-        //   use: {
-          //     loader: 'file-loader',
-          //     options: {
-            //       name: '[name].[ext]',
-            //       outputPath: 'img/',
-            //       publicPath: 'img/'
-            //     }
-            // },
+      //   test: /\.(png|jpg)$/i,
+      //   use: {
+      //     loader: 'file-loader',
+      //     options: {
+      //       name: '[name].[ext]',
+      //       outputPath: 'img/',
+      //       publicPath: 'img/'
+      //     }
+      // },
       // {
         // type: 'asset/resource',
         // generator: {
-          //     filename: '[name]-[hash][ext]'
-          // }
-          // },
-          {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                presets: ['@babel/preset-env', '@babel/preset-react']
-              }
-            }
-          },
-          {
+        //     filename: '[name]-[hash][ext]'
+        // }
+      // },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
         test: /\.s[ac]ss$/i,
         exclude: /node_modules/,
         use: [
@@ -68,6 +68,17 @@ module.exports = {
           "css-loader",
           // Compiles Sass to CSS
           "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
         ],
       },
       {
@@ -84,7 +95,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "My React App 2022",
       template: path.join(__dirname, "src", "index.html"),
-      // template: 'src/index.html',
+      template: 'src/index.html',
       hash: true
     }),
     new HtmlWebpackInlineSVGPlugin({
